4023da6d56e593bedf4f38905a6d67c2fb4f34fc
ple09 <piyathidapinhom@gmail.com>
2022-10-20 21:07:16 -0700

    chang
    555

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("My name is Piyathida");
+        System.out.println("WOW!");
     }
 }
